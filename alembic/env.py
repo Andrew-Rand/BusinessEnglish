b67618c1fdc78f3799e9eb3aecfc7bf9077c3c0e
Andrew_Syman <xseven693@gmail.com>
2022-06-17 14:06:02 +0300

    Add migration functions

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,11 @@ from logging.config import fileConfig
 from alembic import context
 
 from src.db.constants import user, passwd, host, port, db
+from src.basecore.models import Base
+from src.db.db_config import get_engine
+from src.task.models import Task
+from src.user.models import User
+
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,7 +23,53 @@ config.set_main_option("sqlalchemy.url", f"postgresql://{user}:{passwd}@{host}:{
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
+target_metadata = [Base.metadata]
 
-from src.user.models import BaseModel as user_base
-from src.task.models import BaseModel as task_base
-target_metadata = [user_base.metadata, task_base.metadata]
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    db_engine = get_engine()
+
+    with db_engine.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
